6608e5aad4822b1ec2b3659744f06abc100ee494
Ritishree-12 <ritishree1996@gmail.com>
2023-10-05 18:20:02 +0530

    added server and cookies

--- server/server.js
+++ server/server.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken'
 
-const app = express();
+
+
+const app= express();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser())
+app.use(cors(
+ {
+  origin:["http://localhost:3000/registration"],
+  methods:["POST"],
+credentials:true
+ }
+))
 
-// Database Connection
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mydata'
-});
+  host: "localhost",
+  user: "root",
+  password:"",
+  database:"mydata"
+})
 
-// Check if the database connection is successful
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-  } else {
-    console.log('Connected to the database');
-  }
-});
+app.post('/login',(req,res)=>{
+  const sql=" SELECT * FROM login WHERE email = ? AND password = ?";
+  db.query(sql,[req.body.email, req.body.password],(err,data)=>{
+   if(err)return res.json ({message:'server side error'});
+   if(data.length > 0){
+// here we will implement our logic
+// we will generate and also we will store it in a cookie
+//then we will pass them to the frontend result
+// we will grab a name from our record that is existed in our database
+const name= data[0].name;
+const token= jwt.sign({name}, "our-jsonwebtoken-secret-key",{expiresIn:'1d'})
+res.cookie('token', token);
+return res.json({Status:'Success'})
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body; // Destructure email and password from the request body
-  const sql = "SELECT * FROM login WHERE username = ? AND password = ?";
-  db.query(sql, [email, password], (err, data) => {
-    if (err) {
-      console.error('Database error:', err);
-      return res.status(500).json({ error: 'Database error' }); // Handle database error
-    }
-    if (data.length > 0) {
-      return res.json({ message: 'Login successful' });
-    } else {
-      return res.json({ message: 'No records found' });
-    }
-  });
-});
+   }else{
+    return res.json ({Message:'No Record'});
+   }
+  })
+})
 
-app.listen(8081, () => {
-  console.log('Listening on port 8081...');
-});
+app.listen(8081,()=>{
+  console.log('Connected...')
+})
